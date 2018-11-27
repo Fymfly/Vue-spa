44efa771aa2583066ecf1cc83673f418fde4ae1c
fymfly <2944065419@qq.com>
2018-11-27 17:21:30 +0800

    '根据多个ID查询商品-接口'

--- l-shop/app/Http/Controllers/GoodsController.php
+++ l-shop/app/Http/Controllers/GoodsController.php
@@ -10,18 +10,51 @@ class GoodsController extends Controller
     
     public function index(Request $req) {
 
-        $perPage = max(1, (int)$req->per_page);
+        /**
+         * first()  :   取一条记录
+         * get()    :   多条
+         * paginate :   翻页
+         */
 
-        $sortBy = ($req->sortby== 'id' || $req->sortby=='created_at') ? $req->sortby : 'id';
+        
+        if($req->id) {
 
-        $order = ($req->order == 'asc' || $req->order == 'desc') ? $req->order : 'desc';
+            $id = max(1, (int)$req->id);
+            $data = Goods::with('attirubtes','images','skus')
+                    ->where('id', $id)
+                    ->where('is_on_sale','y')
+                    ->first();
 
-        // with : 取出关联表中的数据
-        $data = Goods::with('attirubtes','images','skus')
-                   ->where('goods_name', 'like', '%'.$req->keywords.'%')
-                   ->orderBy($sortBy, $order)
-                   ->paginate( $perPage  );
+            if($data) 
+                return ok($data);
+            else 
+                return error('商品不存在', 404);
 
+        } else if($req->ids) {
+
+            $data = Goods::with('attirubtes','images','skus')
+                        ->whereIn('id', explode(',', $req->ids))
+                        ->where('is_on_sale','y')
+                        ->get();
+            if($data)
+                return ok($data);
+            else
+                return error('商品不存在!', 404);
+                
+        } else {
+
+            // 处理参数
+            $perPage = max(1, (int)$req->per_page);
+            $sortBy = ($req->sortby=='id' || $req->sortby=='created_at') ? $req->sortby : 'id';
+            $order = ($req->order == 'asc' || $req->order == 'desc') ? $req->order : 'desc';
+
+            // with : 取出关联表中的数据
+            $data = Goods::with('attirubtes','images','skus')
+                    ->where('goods_name', 'like', '%'.$req->keywords.'%')
+                    ->where('is_on_sale', 'y')
+                    ->orderBy($sortBy, $order)
+                    ->paginate( $perPage );
+        }
 
         return $data;
     }
